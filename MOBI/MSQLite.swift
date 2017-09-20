72cb6d0077ca0b35a1f5b5f83e77e59fdc135442
Don Michele Bigi <mikbigi@gmail.com>
2017-09-20 18:50:27 +0200

    First version

---
+++ MOBI/MSQLite.swift
@@ -0,0 +1,10 @@
+//
+//  MSQLite.swift
+//  MOBI
+//
+//  Created by Michele Bigi on 20/09/17.
+//  Copyright © 2017 Michele Bigi. All rights reserved.
+//
+
+import Foundation
+import SQLite3
---
+++ MOBISQLite.swift
@@ -0,0 +1,9 @@
+//
+//  MOBISQLite.swift
+//  MOBI
+//
+//  Created by Michele Bigi on 20/09/17.
+//  Copyright © 2017 Michele Bigi. All rights reserved.
+//
+
+import Foundation
