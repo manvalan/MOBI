8ebc9e4e2648c64902a130de3dc8e78841160690
Don Michele Bigi <mikbigi@gmail.com>
2017-09-20 12:39:18 +0200

    Initial Commit

---
+++ MOBI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MOBI
+//
+//  Created by Michele Bigi on 20/09/17.
+//  Copyright © 2017 Michele Bigi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MOBI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MOBI
+//
+//  Created by Michele Bigi on 20/09/17.
+//  Copyright © 2017 Michele Bigi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
