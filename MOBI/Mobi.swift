e7d56271bf34f36765ad67349b331e7c2ff68707
Don Michele Bigi <mikbigi@gmail.com>
2017-09-21 20:21:19 +0200

    mobi.swift

---
+++ MOBI/Mobi.swift
@@ -0,0 +1,20 @@
+//
+//  Mobi.swift
+//  MOBI
+//
+//  Created by Michele Bigi on 21/09/17.
+//  Copyright © 2017 Michele Bigi. All rights reserved.
+//
+
+import Foundation
+
+let NodoTipo0   = 0             // Nodo virtuale
+let NodoTipo1   = 1             // Fermata
+let NodoTipo2   = 2             // Stazione minore
+let NodoTipo3   = 3             // Stazione media
+let NodoTipo4   = 4             // Stazione principale
+let NodoTipo5   = 5             // Stazione di testa
+
+class Mobi {
+   
+}
